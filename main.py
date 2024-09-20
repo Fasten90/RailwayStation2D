c4f51de3768869ffd88ce4cde914121cd708379e
Fasten90 <fasten90@gmail.com>
2024-09-20 23:00:46 +0200

    Train move well

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ import math
 pygame.init()
 vec = pygame.math.Vector2  # 2 for two dimensional
 
-HEIGHT = 480  # TODO: to be calculated
-WIDTH = 640  # TODO: to be calculated
+infoObject = pygame.display.Info()
+
+IS_FULL_SCREEN = True
+if IS_FULL_SCREEN:
+    WIDTH = infoObject.current_w
+    HEIGHT = infoObject.current_h
+else:
+    WIDTH = 640
+    HEIGHT = 480
 
 SCREEN_WIDTH = WIDTH
 
@@ -27,17 +34,22 @@ WHITE = (255, 255, 255)
 
 
 RAIL_TOP_Y = math.floor(HEIGHT/3)
+TRAIN_MOVE_UPPER_Y = 40  # Because the 'rail' image
 TRAIN_SPEED_X = 5
 HIDE_X = -1000
 HIDE_Y = -1000
 
+CONFIG_GAME_NAME = "Vonatos"
  
 FramePerSec = pygame.time.Clock()
  
-DISPLAYSURF = pygame.display.set_mode((WIDTH,HEIGHT))
+if IS_FULL_SCREEN:
+    DISPLAYSURF = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+else:
+    DISPLAYSURF = pygame.display.set_mode((WIDTH,HEIGHT))
 DISPLAYSURF.fill(WHITE)
 
-pygame.display.set_caption("Game")
+pygame.display.set_caption(CONFIG_GAME_NAME)
 
 
 color1 = pygame.Color(0, 0, 0)         # Black
@@ -50,8 +62,10 @@ class Background(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
         self.image = pygame.image.load("images/rail.PNG")
+        #size_x, size_y = self.image.get_size()
+        #self.image = pygame.transform.scale(self.image, (WIDTH, size_y))  # TODO: Don't know how to scale up an image like a texture...
         self.rect = self.image.get_rect()
-        self.rect.center=(0,RAIL_TOP_Y)
+        self.rect.bottomleft=(0,RAIL_TOP_Y)  # Typically the center() recommended
 
     def draw(self, surface):
         surface.blit(self.image, self.rect) 
@@ -63,7 +77,7 @@ class Train(pygame.sprite.Sprite):
         super().__init__() 
         self.image = pygame.image.load("images/locomotive.PNG")
         self.rect = self.image.get_rect()
-        self.rect.center=(RAIL_TOP_Y, 0)
+        self.rect.bottomleft=(0, RAIL_TOP_Y - TRAIN_MOVE_UPPER_Y)
         self.size_x, self.size_y = self.image.get_size()
 
     def move(self):
@@ -99,7 +113,6 @@ class Train(pygame.sprite.Sprite):
 #              if pressed_keys[K_RIGHT]:
 #                  self.rect.move_ip(5, 0)
 
- 
 
 background = Background()
 train_top = Train()
